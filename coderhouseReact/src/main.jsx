0486598550d65b68e4bd9b7f1f825565666661fc
Jonatan Guinea <jonatanguinea@outlook.com>
2024-07-08 21:43:57 -0300

    prueba

--- coderhouseReact/src/main.jsx
+++ coderhouseReact/src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'  //Upper camelCase for components
+import App from './App.jsx'
 import './index.css'
 
-ReactDOM.createRoot(document.querySelector('#root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
